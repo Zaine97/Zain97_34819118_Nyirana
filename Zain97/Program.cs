ccee0bc0124d4784b30138414135a3f2507ee4d2
Zain Nyirana <34819118@mynwu.ac.za>
2023-08-31 14:34:05 +0200

    I commit

--- Zain97/Program.cs
+++ Zain97/Program.cs
@@ -1,11 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Zain97.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
+
+var connectionString = builder.Configuration.GetConnectionString("Defaultconnection");
+builder.Services.AddDbContext<Cmpg323_Nyirana34819118_Project2Context>(options => options.UseSqlServer(connectionString));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
